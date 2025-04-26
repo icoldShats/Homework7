116f0e59032065449a01aacadfcd33056ae0ccfe
icoldShats <medina.sagudenova@narxoz.kz>
2025-04-26 18:57:33 +0600

    plane drops everything and lands first

--- AirportTowerSimulator/ControlTower.java
+++ AirportTowerSimulator/ControlTower.java
@@ -7,4 +7,18 @@ public class ControlTower implements TowerMediator{
     private Queue<Aircraft> landingQueue = new LinkedList<>();
     private Queue<Aircraft> takeoffQueue = new LinkedList<>();
     private boolean runwayBusy = false;
+
+
+    public boolean requestRunway(Aircraft a) {
+        if (!runwayBusy) {
+            runwayBusy = true;
+            System.out.println("Runway granted to " + a.id);
+            return true;
+        }
+        return false;
+    }
+
+    public void releaseRunway() {
+        runwayBusy = false;
+    }
 }
