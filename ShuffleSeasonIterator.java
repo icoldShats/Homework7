564dc4ad86cb1309a5ee1732634ad593ad086898
icoldShats <medina.sagudenova@narxoz.kz>
2025-04-26 17:18:13 +0600

    implement

--- ShuffleSeasonIterator.java
+++ ShuffleSeasonIterator.java
@@ -2,4 +2,14 @@ import java.util.List;
 public class ShuffleSeasonIterator implements EpisodeIterator{
     private List<Episode> shuffled;
     private int index = 0;
+
+    public boolean hasNext() {
+        return index < shuffled.size();
+    }
+
+    public Episode next() {
+        return shuffled.get(index++);
+    }
+
+
 }
