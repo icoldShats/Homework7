92697cee1c06af5ce1bcb85cb8c77ed713ea238c
icoldShats <medina.sagudenova@narxoz.kz>
2025-04-26 16:06:56 +0600

    add constructor for episode

--- Episode.java
+++ Episode.java
@@ -1,3 +1,16 @@
 class Episode {
     String title;
-    int runtimeSec;  }
+    int runtimeSec;
+    public Episode(String title, int runtimeSec) {
+        this.title = title;
+        this.runtimeSec = runtimeSec;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getRuntimeSec() {
+        return runtimeSec;
+    }
+}
