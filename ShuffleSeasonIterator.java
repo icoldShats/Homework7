a2e6c97c3bd2719705c463fddb631b9558cf506f
icoldShats <medina.sagudenova@narxoz.kz>
2025-04-26 17:46:29 +0600

    creating a shuffled list of episodes for random viewin

--- ShuffleSeasonIterator.java
+++ ShuffleSeasonIterator.java
@@ -1,7 +1,17 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+
 public class ShuffleSeasonIterator implements EpisodeIterator{
     private List<Episode> shuffled;
     private int index = 0;
+
+    public ShuffleSeasonIterator(Season season, long seed) {
+        shuffled = new ArrayList<>(season.getEpisodes());
+        Collections.shuffle(shuffled, new Random(seed));
+    }
 
     public boolean hasNext() {
         return index < shuffled.size();
