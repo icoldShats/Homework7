4029e210e8f0119f269073d6daa2af535aaef6b7
icoldShats <medina.sagudenova@narxoz.kz>
2025-04-26 17:19:47 +0600

    implement

--- ReverseSeasonIterator.java
+++ ReverseSeasonIterator.java
@@ -2,4 +2,12 @@ import java.util.List;
 public class ReverseSeasonIterator implements EpisodeIterator{
     private List<Episode> episodes;
     private int index;
+
+    public boolean hasNext() {
+        return index >= 0;
+    }
+
+    public Episode next() {
+        return episodes.get(index--);
+    }
 }
