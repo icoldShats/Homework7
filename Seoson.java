ec92670afd13b541205cd7b562dae5c1ad4a81fc
icoldShats <medina.sagudenova@narxoz.kz>
2025-04-26 16:33:43 +0600

    import our list and init

--- Seoson.java
+++ Seoson.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 class Season implements Iterable<Episode>  {
     private List<Episode> episodes = new ArrayList<>();
     public void addEpisode(Episode e) {
         episodes.add(e);
+    }
+    public List<Episode> getEpisodes() {
+        return episodes;
     }
 
 }
