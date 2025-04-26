893b55be2891fea9e4e9ea01ad7651f928caa41f
icoldShats <medina.sagudenova@narxoz.kz>
2025-04-26 16:56:48 +0600

    Create Main and use our patter

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Season s1 = new Season();
+        s1.addEpisode(new Episode("S1E1", 1200));
+        s1.addEpisode(new Episode("S1E2", 1250));
+        Season s2 = new Season();
+        s2.addEpisode(new Episode("S2E1", 1300));
+        s2.addEpisode(new Episode("S2E2", 1350));
+
+        Series series = new Series();
+        series.addSeason(s1);
+        series.addSeason(s2);
+
+}}
