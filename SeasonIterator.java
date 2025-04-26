6e26d17c6cc926e9b17767b64477236d73c6f035
icoldShats <medina.sagudenova@narxoz.kz>
2025-04-26 17:07:31 +0600

    Creates a private index variable and constructor of the SeasonIterator class

--- SeasonIterator.java
+++ SeasonIterator.java
@@ -2,4 +2,9 @@ import java.util.Iterator;
 import java.util.List;
 class SeasonIterator implements EpisodeIterator, Iterator<Episode> {
     private List<Episode> episodes;
+    private int index = 0;
+
+    public SeasonIterator(Season season) {
+        this.episodes = season.getEpisodes();
+    }
 }
